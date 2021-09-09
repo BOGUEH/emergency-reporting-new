214b4769501440a3a4c64b568ee76dbef253609f
Nelson Ogueh <nelsonovovwe@gmail.com>
2021-09-09 16:00:40 +0100

    working state..

--- src/Components/Admindb/AdminDashboard.js
+++ src/Components/Admindb/AdminDashboard.js
@@ -1,148 +1,146 @@
 import React from 'react'
 import './Admin.css'
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from 'react'
 import axios from 'axios'
 import DashNavBar from '../DashBoard/DashNavBar'
-import { Link } from "react-router-dom";
-import { Table } from 'react-bootstrap'
+import {Link} from "react-router-dom";
+import {Table} from 'react-bootstrap'
 
 
 export default function AdminDashboard() {
     const [adminData, setAdminData] = useState([])
-    const   [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(true)
 
 
-    useEffect(()=> {
+    useEffect(() => {
 
         axios({
             method: 'GET',
-            url:  `https://emergency-report-app.herokuapp.com/api/profile/allprofiles`
-        }).then((res)=> {
+            url: `https://emergency-report-app.herokuapp.com/api/profile/allprofiles`
+        }).then((res) => {
 
 
             setAdminData(res.data.data)
             console.log(adminData);
             console.log(res.data.data);
-        } )
+        })
 
-    },[])
+    }, [])
 
 
-      const setData = (_id, address, institution, phoneNo, slug)=>{
-          console.log(_id, address,institution, phoneNo, slug);
-      localStorage.setItem('ID', _id)
-      localStorage.setItem('Address', address)
-      localStorage.setItem('Institution', institution)
-      localStorage.setItem('PhoneNo', phoneNo)
-      localStorage.setItem('slogan', slug)
-      }
+    const setData = (_id, address, institution, phoneNo, slug) => {
+        console.log(_id, address, institution, phoneNo, slug);
+        localStorage.setItem('ID', _id)
+        localStorage.setItem('Address', address)
+        localStorage.setItem('Institution', institution)
+        localStorage.setItem('PhoneNo', phoneNo)
+        localStorage.setItem('slogan', slug)
+    }
 
-      const getData = ()=>{
+    const getData = () => {
         axios({
             method: 'GET',
-            url:  `https://emergency-report-app.herokuapp.com/api/profile/allprofiles`
-        }).then((res)=> {
+            url: `https://emergency-report-app.herokuapp.com/api/profile/allprofiles`
+        }).then((res) => {
 
 
             setAdminData(res.data.data)
             console.log(adminData);
-        } )
+        })
 
     }
-       const  onDelete = (_id)=>{
-           axios({
+    const onDelete = (_id) => {
+        axios({
 
             method: 'delete',
-          url: `https://emergency-report-app.herokuapp.com/api/profile/delete/${_id}`,
-          headers: {
-        'Authorization': `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2MTMzNTdkY2UxMGJjZjA3MjhlNWJkMzciLCJpYXQiOjE2MzA3NTUxNzgsImV4cCI6MTYzMzM0NzE3OH0.rQt9linzcIk9_WyqxvSahKl9B6Fhss9ioGY_dv9cc8E`
-          }
-           }).then(()=>{
-               getData();
-            })
+            url: `https://emergency-report-app.herokuapp.com/api/profile/delete/${_id}`,
+            headers: {
+                'Authorization': `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2MTMzNTdkY2UxMGJjZjA3MjhlNWJkMzciLCJpYXQiOjE2MzA3NTUxNzgsImV4cCI6MTYzMzM0NzE3OH0.rQt9linzcIk9_WyqxvSahKl9B6Fhss9ioGY_dv9cc8E`
+            }
+        }).then(() => {
+            getData();
+        })
 
-        }
+    }
 
-       let counter = 1;
-
-
+    let counter = 1;
 
 
     return (
 
         <div>
-         <DashNavBar/>
+            <DashNavBar/>
 
 
+            <Table striped bordered hover>
+                <thead>
+                <tr>
+
+                    <th scope="col"> S/N</th>
+                    <th scope="col"> Address</th>
+                    <th scope="col">Institution</th>
+                    <th scope="col">Local Government Area</th>
+                    <th scope="col">Phone No</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">Action</th>
+                </tr>
+                </thead>
+            </Table>
 
 
-                        <Table striped bordered hover>
-                            <thead>
-                                <tr>
+            {
+                adminData.map((user, i) => {
+                        return (
+                            <div key={i} className="Deal Note">
+                                <Table striped bordered hover>
 
-                                <th scope="col"> S/N</th>
-                                <th scope="col"> Address</th>
-                                <th scope="col">Institution</th>
-                                <th scope="col">Local Government Area</th>
-                                <th scope="col">Phone No</th>
-                                <th scope="col">Slug</th>
-                                <th scope="col">Action</th>
-                                </tr>
-                            </thead>
-                     </Table>
+                                    <tbody>
+                                    <tr key={i} className="Deal Note">
 
+                                        <td>{counter++}</td>
+                                        <td>
+                                            {user.address}
+                                        </td>
+                                        <td>
+                                            {user.institution}
+                                        </td>
+                                        <td>
+                                            {user.localGovtArea}
 
+                                        </td>
+                                        <td>
+                                            {user.phoneNo}
 
-             {
-                  adminData.map((user, i)=> {
-                      return(
-                    <div key ={i} className="Deal Note">
-                 <Table striped bordered hover>
+                                        </td>
+                                        <td>
+                                            {user.slug}
 
-                            <tbody>
-                                <tr key ={i} className="Deal Note">
+                                        </td>
+                                        <td>
+                                            <Link to='./update'>
+                                                <button className="btn btn-success"
+                                                        onClick={() => setData(user._id, user.address,
+                                                            user.institution, user.phoneNo, user.slug)}>Update
+                                                </button>
+                                            </Link>
 
-                                <td>{counter ++}</td>
-                                <td>
-                                    {user.address}
-                                </td>
-                                <td>
-                                    {user.institution}
-                                </td>
-                                <td>
-                                    {user.localGovtArea}
+                                            <button className="btn btn-danger"
+                                                    onClick={() => onDelete(user._id)}
+                                            >Delete
+                                            </button>
 
-                                </td>
-                                <td>
-                                    {user.phoneNo}
+                                        </td>
 
-                                </td>
-                                <td>
-                                    {user.slug}
+                                    </tr>
+                                    </tbody>
+                                </Table>
 
-                                </td>
-                                <td>
-                                    <Link to =  './update'>
-                                    <button className="btn btn-success"
-                                    onClick={()=>setData(user._id, user.address,
-                                    user.institution, user.phoneNo, user.slug)}>Update</button>
-                                    </Link>
+                            </div>
 
-                                    <button className="btn btn-danger"
-                                    onClick={()=>onDelete(user._id)}
-                                    >Delete</button>
-
-                                </td>
-
-                                </tr>
-                            </tbody>
-                            </Table>
-
-                 </div>
-
-              )}
-            )
-        }
+                        )
+                    }
+                )
+            }
 
 
         </div>
