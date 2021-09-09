f3fa7bcdb0da6e43ad6be17275b7dcd103c5965b
Nelson Ogueh <nelsonovovwe@gmail.com>
2021-09-09 15:53:13 +0100

    Fix: HTTP-related problem fixed

--- src/Components/Admindb/AdminDashboard.js
+++ src/Components/Admindb/AdminDashboard.js
@@ -11,20 +11,24 @@ export default function AdminDashboard() {
     const [adminData, setAdminData] = useState([])
     const   [loading, setLoading] = useState(true)
 
+
     useEffect(()=> {
 
         axios({
             method: 'GET',
-            url:  `http://emergency-report-app.herokuapp.com/api/profile/allprofiles`
+            url:  `https://emergency-report-app.herokuapp.com/api/profile/allprofiles`
         }).then((res)=> {
-           
-            
+
+
             setAdminData(res.data.data)
             console.log(adminData);
+            console.log(res.data.data);
         } )
-        
+
     },[])
-      const setData = (_id, address, institution, phoneNo, slug)=>{ 
+
+
+      const setData = (_id, address, institution, phoneNo, slug)=>{
           console.log(_id, address,institution, phoneNo, slug);
       localStorage.setItem('ID', _id)
       localStorage.setItem('Address', address)
@@ -36,10 +40,10 @@ export default function AdminDashboard() {
       const getData = ()=>{
         axios({
             method: 'GET',
-            url:  `http://emergency-report-app.herokuapp.com/api/profile/allprofiles`
+            url:  `https://emergency-report-app.herokuapp.com/api/profile/allprofiles`
         }).then((res)=> {
-           
-            
+
+
             setAdminData(res.data.data)
             console.log(adminData);
         } )
@@ -49,19 +53,19 @@ export default function AdminDashboard() {
            axios({
 
             method: 'delete',
-          url: `http://emergency-report-app.herokuapp.com/api/profile/delete/${_id}`,
+          url: `https://emergency-report-app.herokuapp.com/api/profile/delete/${_id}`,
           headers: {
         'Authorization': `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2MTMzNTdkY2UxMGJjZjA3MjhlNWJkMzciLCJpYXQiOjE2MzA3NTUxNzgsImV4cCI6MTYzMzM0NzE3OH0.rQt9linzcIk9_WyqxvSahKl9B6Fhss9ioGY_dv9cc8E`
           }
            }).then(()=>{
                getData();
             })
-             
+
         }
 
        let counter = 1;
-           
-      
+
+
 
 
     return (
@@ -69,13 +73,13 @@ export default function AdminDashboard() {
         <div>
          <DashNavBar/>
 
-         
-                
+
+
 
                         <Table striped bordered hover>
                             <thead>
                                 <tr>
-                               
+
                                 <th scope="col"> S/N</th>
                                 <th scope="col"> Address</th>
                                 <th scope="col">Institution</th>
@@ -85,19 +89,19 @@ export default function AdminDashboard() {
                                 <th scope="col">Action</th>
                                 </tr>
                             </thead>
-                     </Table> 
-                       
-                        
+                     </Table>
+
+
 
              {
                   adminData.map((user, i)=> {
                       return(
                     <div key ={i} className="Deal Note">
                  <Table striped bordered hover>
-                       
+
                             <tbody>
                                 <tr key ={i} className="Deal Note">
-                                
+
                                 <td>{counter ++}</td>
                                 <td>
                                     {user.address}
@@ -107,40 +111,40 @@ export default function AdminDashboard() {
                                 </td>
                                 <td>
                                     {user.localGovtArea}
-                    
+
                                 </td>
                                 <td>
                                     {user.phoneNo}
 
                                 </td>
-                                <td> 
+                                <td>
                                     {user.slug}
 
                                 </td>
-                                <td> 
+                                <td>
                                     <Link to =  './update'>
                                     <button className="btn btn-success"
-                                    onClick={()=>setData(user._id, user.address, 
+                                    onClick={()=>setData(user._id, user.address,
                                     user.institution, user.phoneNo, user.slug)}>Update</button>
                                     </Link>
-                               
+
                                     <button className="btn btn-danger"
                                     onClick={()=>onDelete(user._id)}
                                     >Delete</button>
 
                                 </td>
-                                
+
                                 </tr>
                             </tbody>
-                            </Table> 
-       
+                            </Table>
+
                  </div>
 
               )}
             )
         }
-          
-        
+
+
         </div>
     )
 }
